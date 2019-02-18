57f462e6ff3f581aeb8ad670f8dca5548a336d48
Kumar Abhinav <abhinav0441@gmail.com>
2019-02-18 11:21:58 +0530

    ssss

--- 1-typescript/arrays.js
+++ 1-typescript/arrays.js
@@ -10,3 +10,13 @@ for (var i = 0; i < fibNumbers.length; i++) {
     console.log(fibNumbers[i] * 2);
 }
 fibNumbers.forEach(function (e) { return console.log(e * 2); });
+var dblFibNumbers = fibNumbers.map(function (n) { return n * 2; });
+console.log(dblFibNumbers);
+var evenFibNumbers = fibNumbers.filter(function (n) { return n % 2 == 0; });
+console.log(evenFibNumbers);
+var sum = fibNumbers.reduce(function (res, n) { return res + n; }, 0);
+console.log(sum);
+var prod = fibNumbers.reduce(function (res, n) { return res * n; }, 1);
+var numbers = [32, 1, 44, 77, 2, 3, 7];
+var largest = numbers.reduce(function (res, n) { return res > n ? res : n; }, -Infinity);
+console.log(largest);
--- 1-typescript/arrays.ts
+++ 1-typescript/arrays.ts
@@ -14,3 +14,20 @@ for(let i = 0; i < fibNumbers.length; i++) {
 }
 
 fibNumbers.forEach( e => console.log(e*2) );
+
+let dblFibNumbers = fibNumbers.map( n => n*2 );
+console.log(dblFibNumbers);
+
+let evenFibNumbers = fibNumbers.filter( n => n%2 == 0 );
+console.log(evenFibNumbers);
+
+let sum = fibNumbers.reduce( (res, n) => res + n, 0);
+console.log(sum);
+
+let prod = fibNumbers.reduce( (res, n) => res*n, 1);
+
+let numbers = [32,1,44,77,2,3,7];
+
+let largest = numbers.reduce((res, n) => res > n ? res : n, numbers[0] );
+console.log(largest);
+
